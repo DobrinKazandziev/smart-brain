fe3665b6888d0261c49fbcd6751401207476b7ee
DobrinKazandziev <dobrinkazandziev94@gmail.com>
2019-06-27 16:23:33 +0200

    update

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -18,7 +18,7 @@ class SignIn extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('http://localhost:3000/signin', {
+        fetch('https://quiet-sea-60172.herokuapp.com/signin', {
            method: 'post',
            headers: {'Content-Type': 'application/json'},
            body: JSON.stringify({
